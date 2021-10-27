301cfda4f60a3c10db5cf72e39e50db36e27907d
Monir <mknishad1@ymail.com>
2021-10-27 13:12:59 +0600

    show progress dialog

--- libs/sdk-v2/src/main/java/com/shurjopay/sdk/v2/payment/PaymentActivity.kt
+++ libs/sdk-v2/src/main/java/com/shurjopay/sdk/v2/payment/PaymentActivity.kt
@@ -1,5 +1,6 @@
 package com.shurjopay.sdk.v2.payment
 
+import android.app.ProgressDialog
 import android.net.http.SslError
 import android.os.Bundle
 import android.util.Log
@@ -12,10 +13,7 @@ import com.shurjopay.sdk.v2.databinding.ActivityPaymentBinding
 import com.shurjopay.sdk.v2.model.*
 import com.shurjopay.sdk.v2.networking.ApiClient
 import com.shurjopay.sdk.v2.networking.ApiInterface
-import com.shurjopay.sdk.v2.utils.DATA
-import com.shurjopay.sdk.v2.utils.PAYMENT_CANCELLED
-import com.shurjopay.sdk.v2.utils.PAYMENT_CANCELLED_BY_USER
-import com.shurjopay.sdk.v2.utils.SDK_TYPE
+import com.shurjopay.sdk.v2.utils.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,9 +22,10 @@ class PaymentActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityPaymentBinding
 
+  private lateinit var progressDialog: ProgressDialog
+
   private lateinit var sdkType: String
   private lateinit var data: RequiredData
-
   private var tokenResponse: Token? = null
   private var checkoutRequest: CheckoutRequest? = null
   private var checkoutResponse: CheckoutResponse? = null
@@ -36,14 +35,17 @@ class PaymentActivity : AppCompatActivity() {
     binding = ActivityPaymentBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    progressDialog = ProgressDialog(this)
+
     sdkType = intent.getStringExtra(SDK_TYPE).toString()
     data = intent.getParcelableExtra(DATA)!!
-    Log.d(TAG, "onCreate: requiredDataModel = $data")
+    Log.d(TAG, "onCreate: data = $data")
 
     getToken()
   }
 
   private fun getToken() {
+    showProgress()
     val token = Token(
       data.username, data.password, null, null, null,
       null, null, null, null
@@ -58,11 +60,18 @@ class PaymentActivity : AppCompatActivity() {
               tokenResponse = response.body()
               getExecuteUrl()
             }
+          } else {
+            hideProgress()
+            ShurjoPaySDK.listener?.onFailed(PAYMENT_DECLINED)
+            finish()
           }
         }
 
         override fun onFailure(call: Call<Token>, t: Throwable) {
           Log.e(TAG, "onFailure: ${t.message}", t)
+          hideProgress()
+          ShurjoPaySDK.listener?.onFailed(PAYMENT_DECLINED)
+          finish()
         }
       })
   }
@@ -100,14 +109,21 @@ class PaymentActivity : AppCompatActivity() {
     )?.enqueue(object : Callback<CheckoutResponse> {
       override fun onResponse(call: Call<CheckoutResponse>, response: Response<CheckoutResponse>) {
         Log.d(TAG, "onResponse: ${response.body()}")
+        hideProgress()
         if (response.isSuccessful) {
           checkoutResponse = response.body()
           setupWebView()
+        } else {
+          ShurjoPaySDK.listener?.onFailed(PAYMENT_DECLINED)
+          finish()
         }
       }
 
       override fun onFailure(call: Call<CheckoutResponse>, t: Throwable) {
         Log.e(TAG, "onFailure: ${t.message}", t)
+        hideProgress()
+        ShurjoPaySDK.listener?.onFailed(PAYMENT_DECLINED)
+        finish()
       }
     })
   }
@@ -128,23 +144,6 @@ class PaymentActivity : AppCompatActivity() {
         if (url.contains("return_url") && url.contains("order_id")) {
           verifyPayment()
         }
-
-        /*previousUrl.get(0) = currentUrl.get(0)
-        currentUrl.get(0) = url
-        Log.d(TAG, "shouldOverrideUrlLoading: previousUrl[0] = " + previousUrl.get(0))
-        Log.d(TAG, "shouldOverrideUrlLoading: currentUrl[0] = " + currentUrl.get(0))
-        if (currentUrl.get(0).contains("return_url.php")) {
-          //ShurjoPaySDK.listener.onFailed(SPayConstants.Exception.PAYMENT_CANCELLED_BY_USER);
-          //finish();
-          if (previousUrl.get(0).contains("cancel=ok")) {
-            ShurjoPaySDK.listener!!.onFailed(SPayConstants.Exception.PAYMENT_CANCELLED)
-            finish()
-          } else {
-            //getTransactionInfo()
-          }
-        } else {
-          view.loadUrl(url)
-        }*/
         return false
       }
 
@@ -160,6 +159,7 @@ class PaymentActivity : AppCompatActivity() {
   }
 
   private fun verifyPayment() {
+    showProgress()
     val transactionInfo = TransactionInfo(
       null,
       checkoutResponse?.sp_order_id!!,
@@ -198,10 +198,8 @@ class PaymentActivity : AppCompatActivity() {
     )?.enqueue(object : Callback<List<TransactionInfo>> {
       override fun onResponse(call: Call<List<TransactionInfo>>, response: Response<List<TransactionInfo>>) {
         Log.d(TAG, "onResponse: ${response.body()}")
+        hideProgress()
         if (response.isSuccessful) {
-          //checkoutResponse = response.body()
-          //setupWebView()
-
           if (response.body()?.get(0)?.sp_code == 1000) {
             ShurjoPaySDK.listener?.onSuccess(response.body()?.get(0))
             finish()
@@ -214,8 +212,23 @@ class PaymentActivity : AppCompatActivity() {
 
       override fun onFailure(call: Call<List<TransactionInfo>>, t: Throwable) {
         Log.e(TAG, "onFailure: ${t.message}", t)
+        hideProgress()
+        ShurjoPaySDK.listener?.onFailed(PLEASE_CHECK_YOUR_PAYMENT)
+        finish()
       }
     })
+  }
+
+  private fun showProgress() {
+    progressDialog.setMessage("Please Wait...")
+    progressDialog.setCancelable(false)
+    progressDialog.show()
+  }
+
+  private fun hideProgress() {
+    if (progressDialog.isShowing) {
+      progressDialog.dismiss()
+    }
   }
 
   override fun onBackPressed() {
