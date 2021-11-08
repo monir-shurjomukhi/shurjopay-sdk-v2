6a9e4a5fbc67543048b587df14e91efcf462a309
Monir <mknishad1@ymail.com>
2021-11-08 13:33:15 +0600

    enable proguard, remove okhttp logger

--- libs/sdk-v2/src/main/java/com/shurjopay/sdk/v2/networking/ApiClient.kt
+++ libs/sdk-v2/src/main/java/com/shurjopay/sdk/v2/networking/ApiClient.kt
@@ -2,27 +2,14 @@ package com.shurjopay.sdk.v2.networking
 
 import com.google.gson.GsonBuilder
 import com.shurjopay.sdk.v2.utils.*
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 class ApiClient {
   private var retrofit: Retrofit? = null
 
   fun getApiClient(sdkType: String): Retrofit? {
     if (retrofit == null) {
-      val interceptor = HttpLoggingInterceptor()
-      interceptor.level = HttpLoggingInterceptor.Level.BODY
-
-      val okHttpClient: OkHttpClient = OkHttpClient.Builder()
-        .connectTimeout(60, TimeUnit.SECONDS)
-        .writeTimeout(60, TimeUnit.SECONDS)
-        .readTimeout(60, TimeUnit.SECONDS)
-        .addInterceptor(interceptor)
-        .build()
-
       val gson = GsonBuilder().setLenient().create()
 
       var baseUrl = BASE_URL_SANDBOX
@@ -44,7 +31,6 @@ class ApiClient {
       retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create(gson))
-        .client(okHttpClient)
         .build()
     }
     return retrofit
